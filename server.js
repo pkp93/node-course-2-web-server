ed40ecd337ba9117c46f2319e4e6d395ba3be769
Kumar <pkumar7@techteam.com>
2018-08-28 19:55:45 +0530

    Setup script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs=require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -15,7 +16,7 @@ app.use((req, res, next) => {
   fs.appendFile('Server.log',log + '\n', (err) => {
     if(err) {
       console.log('Unable to append to server.log');
-    }
+    };
   });
   next();
 });
@@ -72,4 +73,8 @@ app.get('/bad',(req,res) => {
   });
 });
 
-app.listen(3000);   //binds the app to a port in our machine.
+
+
+app.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});   //binds the app to a port in our machine.
